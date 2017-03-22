adbfefa97a9e710c30429017132b2e9c8011ddfe
Florian da Costa <florian.dacosta@akretion.com>
2017-03-22 15:38:41 +0100

    Fix context in sale product_id_change

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -865,6 +865,7 @@ class sale_order_line(osv.osv):
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
             uom=False, qty_uos=0, uos=False, name='', partner_id=False,
             lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
+        original_context = context.copy()
         context = context or {}
         lang = lang or context.get('lang',False)
         if not  partner_id:
@@ -949,11 +950,13 @@ class sale_order_line(osv.osv):
                     'Please set one before choosing a product.')
             warning_msgs += _("No Pricelist ! : ") + warn_msg +"\n\n"
         else:
+            ctx = original_context.copy()
+            ctx.update({
+                'uom': uom or result.get('product_uom'),
+                'date': date_order,
+            })
             price = self.pool.get('product.pricelist').price_get(cr, uid, [pricelist],
-                    product, qty or 1.0, partner_id, {
-                        'uom': uom or result.get('product_uom'),
-                        'date': date_order,
-                        })[pricelist]
+                    product, qty or 1.0, partner_id, context=ctx)[pricelist]
             if price is False:
                 warn_msg = _("Cannot find a pricelist line matching this product and quantity.\n"
                         "You have to change either the product, the quantity or the pricelist.")
