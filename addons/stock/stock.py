15592f66a26e427185c048be0cd3fdfa68498a31
Florian da Costa <florian.dacosta@akretion.com>
2017-07-13 13:07:31 +0200

    Apply fix from core to copy picking origin in some cases

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -750,9 +750,8 @@ class stock_picking(osv.osv):
         if ('name' not in default) or (picking_obj.name == '/'):
             seq_obj_name = 'stock.picking.' + picking_obj.type
             default['name'] = self.pool.get('ir.sequence').get(cr, uid, seq_obj_name)
-            default['backorder_id'] = False
-        if 'origin' not in default:
-            default['origin'] = ''
+            default.setdefault('origin', False)
+            default.setdefault('backorder_id', False)
         if 'invoice_state' not in default and picking_obj.invoice_state == 'invoiced':
             default['invoice_state'] = '2binvoiced'
         res = super(stock_picking, self).copy(cr, uid, id, default, context)
