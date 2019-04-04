c8b85e5763256ae56cd6f692508a23710b5ec443
Benoit <benoit.guillot@akretion.com>
2019-04-04 10:56:01 +0200

    [IMP] authorize html tag desc-destockage

--- openerp/tools/mail.py
+++ openerp/tools/mail.py
@@ -46,7 +46,7 @@ tags_to_kill = ["script", "head", "meta", "title", "link", "style", "frame", "if
 tags_to_remove = ['html', 'body', 'font']
 
 # allow new semantic HTML5 tags
-allowed_tags = clean.defs.tags | frozenset('article section header footer hgroup nav aside figure main'.split() + [etree.Comment])
+allowed_tags = clean.defs.tags | frozenset('article section header footer hgroup nav aside figure main desc-destockage'.split() + [etree.Comment])
 safe_attrs = clean.defs.safe_attrs | frozenset(
     ['style',
      'data-oe-model', 'data-oe-id', 'data-oe-field', 'data-oe-type', 'data-oe-expression', 'data-oe-translate', 'data-oe-nodeid',
