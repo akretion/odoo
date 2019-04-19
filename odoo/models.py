19bae98bca32905be29bfabb925d96266ace3929
David Beal <david.beal@akretion.com>
2019-04-19 19:04:07 +0200

    TMP readfromdb warn

--- odoo/models.py
+++ odoo/models.py
@@ -2943,7 +2943,7 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             # mark non-existing records in missing
             forbidden = missing.exists()
             if forbidden:
-                _logger.info(
+                _logger.warning(
                     _('The requested operation cannot be completed due to record rules: Document type: %s, Operation: %s, Records: %s, User: %s') % \
                     (self._name, 'read', ','.join([str(r.id) for r in self][:6]), self._uid))
                 # store an access error exception in existing records
