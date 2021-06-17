21e336f0ef90e13cda691a2867694f27cf78ab85
David Beal <david.beal@akretion.com>
2021-06-17 18:34:02 +0200

    IMP make upper paging for o2m view

--- addons/web/static/src/js/views/form/form_view.js
+++ addons/web/static/src/js/views/form/form_view.js
@@ -136,7 +136,7 @@ var FormView = BasicView.extend({
     _setSubViewLimit: function (attrs) {
         var view = attrs.views && attrs.views[attrs.mode];
         var limit = view && view.arch.attrs.limit && parseInt(view.arch.attrs.limit, 10);
-        attrs.limit = limit || attrs.Widget.prototype.limit || 40;
+        attrs.limit = limit || attrs.Widget.prototype.limit || 100;
     },
 });
 
