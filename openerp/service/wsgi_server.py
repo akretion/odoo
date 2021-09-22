85cfb3eb98065f8961fb6e7a3e7ae8437854b4e1
Benoit <benoit.guillot@akretion.com>
2021-09-22 11:15:46 +0200

    remove newrelic for new prod

--- openerp/service/wsgi_server.py
+++ openerp/service/wsgi_server.py
@@ -44,9 +44,7 @@ import werkzeug.contrib.fixers
 import openerp
 import openerp.tools.config as config
 import websrv_lib
-import newrelic.agent
 
-newrelic.agent.initialize()
 
 _logger = logging.getLogger(__name__)
 
@@ -212,7 +210,6 @@ def application_unproxied(environ, start_response):
     start_response('404 Not Found', [('Content-Type', 'text/plain'), ('Content-Length', str(len(response)))])
     return [response]
 
-@newrelic.agent.wsgi_application()
 def application(environ, start_response):
     if config['proxy_mode'] and 'HTTP_X_FORWARDED_HOST' in environ:
         return werkzeug.contrib.fixers.ProxyFix(application_unproxied)(environ, start_response)
