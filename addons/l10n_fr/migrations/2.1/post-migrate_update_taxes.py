69b049dca7f31b4dc5dcdf5946062b92e5ccb58e
Sébastien BEAU <sebastien.beau@akretion.com>
2023-10-29 18:32:00 +0100

    l10n_fr: inactive migration

--- addons/l10n_fr/migrations/2.1/post-migrate_update_taxes.py
+++ addons/l10n_fr/migrations/2.1/post-migrate_update_taxes.py
@@ -3,4 +3,6 @@ from odoo.addons.account.models.chart_template import update_taxes_from_template
 
 
 def migrate(cr, version):
+    # DO NOT DO SHIT
+    return
     update_taxes_from_templates(cr, 'l10n_fr.l10n_fr_pcg_chart_template')
