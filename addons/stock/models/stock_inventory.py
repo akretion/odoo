837fad015303e80190ab4bf41eefb1802ebca2e2
Sébastien BEAU <sebastien.beau@akretion.com>
2024-02-05 12:46:05 +0100

    stock: archived product should be included in inventory if stock exists

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -239,8 +239,8 @@ class Inventory(models.Model):
         domain = [('company_id', '=', self.company_id.id),
                   ('quantity', '!=', '0'),
                   ('location_id', 'in', locations_ids)]
-        if self.prefill_counted_quantity == 'zero':
-            domain.append(('product_id.active', '=', True))
+        # if self.prefill_counted_quantity == 'zero':
+        #     domain.append(('product_id.active', '=', True))
 
         if self.product_ids:
             domain = expression.AND([domain, [('product_id', 'in', self.product_ids.ids)]])
